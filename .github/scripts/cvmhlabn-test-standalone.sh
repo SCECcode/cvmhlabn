8d3a5dfa4c37e5e0eb31d377e84f9573b3ef1c4f
Mei-Hui Su <mei@MeiPro.local>
2022-02-04 09:50:23 -0800

    change to tee for test-standalone

--- .github/scripts/cvmhlabn-test-standalone.sh
+++ .github/scripts/cvmhlabn-test-standalone.sh
@@ -2,7 +2,7 @@
 
 cd test_cvmh
 
-make run_unit | test result_unit.txt
+make run_unit | tee result_unit.txt
 
 p=`grep -c FAIL result_unit.txt` 
 if [ $p != 0 ]; then
@@ -13,7 +13,7 @@ else
    exit 0
 fi
 
-make run_accept | test result_accept.txt
+make run_accept | tee result_accept.txt
 
 p=`grep -c FAIL result_accept.txt` 
 if [ $p != 0 ]; then
