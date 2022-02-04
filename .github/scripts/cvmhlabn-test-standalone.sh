c25ff3c0cfc659c0c4bbd384e9df7d6d320085cd
Mei-Hui Su <mei@MeiPro.local>
2022-02-04 09:06:18 -0800

    fix the test

--- .github/scripts/cvmhlabn-test-standalone.sh
+++ .github/scripts/cvmhlabn-test-standalone.sh
@@ -4,10 +4,7 @@ cd test_cvmh
 
 make run_unit | test result_unit.txt
 
-make run_accept | test result_accept.txt
-
-p=`grep -c FAIL result.txt` 
-
+p=`grep -c FAIL result_unit.txt` 
 if [ $p != 0 ]; then
    echo "something wrong.."
    exit 1 
@@ -16,4 +13,13 @@ else
    exit 0
 fi
 
+make run_accept | test result_accept.txt
 
+p=`grep -c FAIL result_accept.txt` 
+if [ $p != 0 ]; then
+   echo "something wrong.."
+   exit 1 
+else
+   echo "okay"
+   exit 0
+fi
