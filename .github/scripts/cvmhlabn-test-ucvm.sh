d1923f6049326cf5e1563aed4a4c33a745184f53
Mei-Hui Su <mei@MeiPro.local>
2022-04-18 15:34:34 -0700

    add -ldl

--- .github/scripts/cvmhlabn-test-ucvm.sh
+++ .github/scripts/cvmhlabn-test-ucvm.sh
@@ -12,12 +12,12 @@ if [ $p != 0 ]; then
    exit 1 
 fi
 
-#make run_validate_with_ucvm | tee result_validate_ucvm.txt
-#p=`grep -c FAIL result_validate_ucvm.txt` 
-#if [ $p != 0 ]; then
-#   echo "something wrong.."
-#   exit 1 
-#fi
+make run_validate_with_ucvm | tee result_validate_ucvm.txt
+p=`grep -c FAIL result_validate_ucvm.txt` 
+if [ $p != 0 ]; then
+   echo "something wrong.."
+   exit 1 
+fi
 
 exit 0 
 
